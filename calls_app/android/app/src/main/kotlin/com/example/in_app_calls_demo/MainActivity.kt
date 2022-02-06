c533991343f7531a3e42da8a88033fe86dbe04be
NikitaPipita <artur.rejet.vseh@gmail.com>
2022-02-06 20:07:45 +0200

    Added Android native notification service

--- calls_app/android/app/src/main/kotlin/com/example/in_app_calls_demo/MainActivity.kt
+++ calls_app/android/app/src/main/kotlin/com/example/in_app_calls_demo/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.in_app_calls_demo
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        NotificationServiceExtension.configureNotificationsMethodChannel(flutterEngine)
+    }
 }
