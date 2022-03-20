9f86216a39effa3a984143654b4964723c99bf8d
NikitaPipita <artur.rejet.vseh@gmail.com>
2022-03-20 21:05:29 +0200

    NotificationService moved to the notifications directory

--- calls_app/android/app/src/main/kotlin/com/example/in_app_calls_demo/notifications/NotificationServiceExtension.kt
+++ calls_app/android/app/src/main/kotlin/com/example/in_app_calls_demo/notifications/NotificationServiceExtension.kt
@@ -1,7 +1,11 @@
-package com.example.in_app_calls_demo
+package com.example.in_app_calls_demo.notifications
 
 import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import com.example.in_app_calls_demo.connection_service.TelecomManagerHelper
+import com.example.in_app_calls_demo.models.CallData
 import com.example.in_app_calls_demo.utils.Constants
 import com.onesignal.OSNotification
 import com.onesignal.OSNotificationReceivedEvent
@@ -26,23 +30,34 @@ class NotificationServiceExtension : OSRemoteNotificationReceivedHandler {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun remoteNotificationReceived(context: Context, notificationReceivedEvent: OSNotificationReceivedEvent) {
         val notification: OSNotification = notificationReceivedEvent.notification
 
         val data = notification.additionalData
         Log.i(tag, "Received Notification Data: $data")
 
-        if (notificationsMethodChannel != null) {
-            if (data["notificationType"] == Constants.CALL_INVITATION) {
-                notificationsMethodChannel!!.invokeMethod(callInvitationNotificationMethod, Notifications.messageToJson(notification))
-                notificationReceivedEvent.complete(null)
-                return
-            } else {
-                notificationsMethodChannel!!.invokeMethod(notificationMethod, Notifications.messageToJson(notification))
-            }
+        val notificationType = data["notificationType"]
+
+        if (notificationType == Constants.CALL_INVITATION) {
+            val telecomManager = TelecomManagerHelper(context)
+            //TODO: add real call data
+            val callData = CallData(callerId = "12345", channelId = "1", callerPhone = "+1 123 1234 12345", callerName = "Name Name", hasVideo = true)
+            telecomManager.makeCall(callData)
+
+            sendNotificationToMethodChannel(callInvitationNotificationMethod, notification)
+            notificationReceivedEvent.complete(null)
+            return
+        } else {
+            sendNotificationToMethodChannel(notificationMethod, notification)
         }
 
         notificationReceivedEvent.complete(notification)
     }
 
+    private fun sendNotificationToMethodChannel(method: String, notification: OSNotification) {
+        if (notificationsMethodChannel != null) {
+            notificationsMethodChannel!!.invokeMethod(method, Notifications.messageToJson(notification))
+        }
+    }
 }
