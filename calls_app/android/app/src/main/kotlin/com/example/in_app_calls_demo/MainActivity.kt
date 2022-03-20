6c0570918e076ce98a2713af2d2785a9eac1d03f
NikitaPipita <artur.rejet.vseh@gmail.com>
2022-03-20 21:06:42 +0200

    Set up CallsApplication while launching and destroying the app

--- calls_app/android/app/src/main/kotlin/com/example/in_app_calls_demo/MainActivity.kt
+++ calls_app/android/app/src/main/kotlin/com/example/in_app_calls_demo/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.in_app_calls_demo
 
+import android.os.Bundle
+import com.example.in_app_calls_demo.notifications.NotificationServiceExtension
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private lateinit var callsApp: CallsApplication
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        callsApp = applicationContext as CallsApplication
+        callsApp.setAppActivity(this)
+    }
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         NotificationServiceExtension.configureNotificationsMethodChannel(flutterEngine)
+    }
+
+    override fun onDestroy() {
+        clearReferences()
+        super.onDestroy()
+    }
+
+    private fun clearReferences() {
+        val appActivity = callsApp.getAppActivity()
+        if (this == appActivity) {
+            callsApp.setAppActivity(null)
+        }
     }
 }
