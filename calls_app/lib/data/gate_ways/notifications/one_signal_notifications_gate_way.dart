d7f407da92c8fda8ce87b37f919f47754c0a6e8a
pipita <artur.rejet.vseh@gmail.com>
2023-05-23 18:44:11 +0300

    Added environment config

--- calls_app/lib/data/gate_ways/notifications/one_signal_notifications_gate_way.dart
+++ calls_app/lib/data/gate_ways/notifications/one_signal_notifications_gate_way.dart
@@ -1,10 +1,12 @@
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
+import '../../../environment_config.dart';
+
 class OneSignalNotificationsGateWay {
   static final _oneSignal = OneSignal.shared;
 
   Future<void> registerUser(String userId) async {
-    await _oneSignal.setAppId('');
+    await _oneSignal.setAppId(EnvironmentConfig.oneSignalKey);
     await _oneSignal.setExternalUserId(userId);
   }
 }
